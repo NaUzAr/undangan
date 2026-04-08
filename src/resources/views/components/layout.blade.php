bd4f93849374aac847fc2d45d72ea7ecedfe1e5e
NaUzAr <nauzar123@gmail.com>
2026-04-08 13:12:50 +0700

    add elegant custom wedding favicon logo

--- src/resources/views/components/layout.blade.php
+++ src/resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Undangan Pernikahan Ahya & Eko</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
